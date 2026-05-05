3dad9ba38086df1d8c32344864d161486ae04a88
Sowmiya R <sowmiyarm848@gmail.com>
2026-05-05 08:37:12 +0530

    Add files via upload

---
+++ append-list-func.py
@@ -0,0 +1,14 @@
+my_list=[1,2,3,4,5,6]
+another_list=[8,9,6]
+#append with an element
+my_list.append(7)
+print(my_list)
+#append with a list
+my_list.append(another_list)
+print(my_list)
+#append with tuple
+my_list.append((10,11))
+print(my_list)
+#append with string
+my_list.append("hello")
+print(my_list)
